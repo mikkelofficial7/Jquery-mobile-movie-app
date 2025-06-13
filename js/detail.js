30d0869709ce89ff4e58296c1c0fff647bfa0919
Mikkel <mikkelseptiano86@gmail.com>
2025-06-14 01:59:36 +0700

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -1,16 +1,18 @@
-const path = window.location.pathname;
-const segments = path.split('/').filter(Boolean);
+document.addEventListener("DOMContentLoaded", function () {
+	const path = window.location.pathname;
+	const segments = path.split('/').filter(Boolean);
 
-if (segments.length === 3 && segments[0] === "detail") {
-	const type = segments[1];
-	const id = segments[2];
+	if (segments.length === 3 && segments[0] === "detail") {
+		const type = segments[1];
+		const id = segments[2];
 
-	console.log("Type:", type);
-	console.log("ID:", id);
+		console.log("Type:", type);
+		console.log("ID:", id);
 
-	// if (type === "movie") {
-	// 	runDetailMovieData(id)
-	// } else if (type === "tv") {
-	// 	runDetailTvShowData(id)
-	// }
-}
+		// if (type === "movie") {
+		// 	runDetailMovieData(id)
+		// } else if (type === "tv") {
+		// 	runDetailTvShowData(id)
+		// }
+	}
+});
